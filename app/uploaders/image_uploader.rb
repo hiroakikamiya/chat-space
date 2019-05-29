1b480da785ccbffc831eff1bfc3b115bbd2580f8
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-05-29 18:44:23 +0900

    image_uploaderの編集

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,4 +44,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+  
+  include CarrierWave::MiniMagick
+
+  process resize_to_fit: [800, 800]
 end
