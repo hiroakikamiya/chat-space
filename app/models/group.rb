75ab09b25e38e124e33880409c516382881b9cf4
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-05-31 15:42:16 +0900

    group.rb編集

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,13 @@ class Group < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   has_many :messages
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.contents? ? last_message.contents : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
 end
