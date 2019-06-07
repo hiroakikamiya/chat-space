4813f45b6d6d0663ffb9f386c6025918a4e430ad
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-06-07 16:55:50 +0900

    Update message_controller.rb

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,8 +1,11 @@
 class Api::MessagesController < ApplicationController
+
   def index
     respond_to do |format| 
       format.html
-      format.json { @messages = Message.where('id > ?', params[:id]) }
+      format.json { @messages = Message.where('id > ? && group_id = ?' , params[:id], params[:group_id] ) }
     end
   end
+
+
 end
